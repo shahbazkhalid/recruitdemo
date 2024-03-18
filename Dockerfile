8b9214fb84e52e5f1d3b8fa35bd8b7d88dfdcc26
Shahbaz Khalid <shahbaz.khalid@gmail.com>
2024-03-18 18:58:51 +0000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use an official Python runtime as a parent image
+FROM python:3.8
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port that Streamlit will run on
+EXPOSE 8501
+
+# Run Streamlit when the container launches
+CMD ["streamlit", "run", "index.py"]
---
+++ index.py
@@ -0,0 +1,60 @@
+import os, streamlit as st
+import openai
+import hmac
+from dotenv import load_dotenv
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+# Main Streamlit app starts here
+
+load_dotenv()  # Load environment variables from .env file
+
+# Access the API key using os.environ
+api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = api_key
+
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+
+# Define a simple Streamlit app
+st.title("Ask Llama")
+query = st.text_input("What would you like to ask? (source: some developer resumes)", "")
+
+# If the 'Submit' button is clicked
+if st.button("Submit"):
+    if not query.strip():
+        st.error(f"Please provide the search query.")
+    else:
+        try:
+            # This example uses text-davinci-003 by default; feel free to change if desired
+            documents = SimpleDirectoryReader("data").load_data()
+            index = VectorStoreIndex.from_documents(documents)
+            query_engine = index.as_query_engine()
+            response = query_engine.query(query)
+            st.success(response)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
