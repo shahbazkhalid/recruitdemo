1f2c00a3cf272abcc03ba3e34ca5b3e180232b0e
Shahbaz Khalid <shahbaz.khalid@gmail.com>
2024-03-18 20:33:56 +0000

    Shifted to ENV file for passwords

--- index.py
+++ index.py
@@ -3,12 +3,15 @@ import openai
 import hmac
 from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables from .env file
+
 def check_password():
     """Returns `True` if the user had the correct password."""
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+        # if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+        if hmac.compare_digest(st.session_state["password"], os.environ.get("password")):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the password.
         else:
@@ -32,8 +35,6 @@ if not check_password():
 
 # Main Streamlit app starts here
 
-load_dotenv()  # Load environment variables from .env file
-
 # Access the API key using os.environ
 api_key = os.environ.get("OPENAI_API_KEY")
 openai.api_key = api_key
@@ -41,8 +42,8 @@ openai.api_key = api_key
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 # Define a simple Streamlit app
-st.title("Ask Llama")
-query = st.text_input("What would you like to ask? (source: some developer resumes)", "")
+st.title("Resume Q/A Demo")
+query = st.text_input("What would you like to ask? (e.g.: Who has python experience?)", "")
 
 # If the 'Submit' button is clicked
 if st.button("Submit"):
